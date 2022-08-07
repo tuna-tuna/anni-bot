ed83dd664ae467164a280fc81f0feca57f050f04
tuna-tuna <bouningen13@gmail.com>
2022-08-07 17:49:22 +0900

    added namemc command

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const mineflayer = require('mineflayer');
 const puppeteer = require('puppeteer');
+const { default: axios } = require('axios');
 const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
 const wait = require('util').promisify(setTimeout);
 require('dotenv').config();
 const { MCUN, MCPW, CLIENT_ID, TOKEN } = process.env
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+let isWorking = false;
 
 client.once('ready', async () => {
     const data = [
@@ -16,6 +19,16 @@ client.once('ready', async () => {
         {
             name: 'stats',
             description: 'View Player\'s stats',
+            options: [{
+                type: 3,
+                name: 'mcid',
+                description: 'Enter mcid.',
+                required: true
+            }]
+        },
+        {
+            name: 'namemc',
+            description: 'View Player\'s name history',
             options: [{
                 type: 3,
                 name: 'mcid',
@@ -33,129 +46,135 @@ client.on('interactionCreate', async (interaction) => {
         return;
     }
     if (interaction.commandName === 'matches') {
-        await interaction.deferReply();
-        let fieldsRaw = [];
-        let fieldRaw = {};
-        let embedObject = {};
-        const getMatch = new Promise((resolve, reject) => {
-            let reopen = false;
-            const bot = mineflayer.createBot({
-                host: 'play.shotbow.net',
-                username: MCUN,
-                password: MCPW,
-                auth: 'microsoft',
-                version: '1.12.2'
-            });
+        if (isWorking === true) {
+            await interaction.reply('Bot is busy now! Try again later!');
+        } else {
+            await interaction.deferReply();
+            let fieldsRaw = [];
+            let fieldRaw = {};
+            let embedObject = {};
+            const getMatch = new Promise((resolve, reject) => {
+                let reopen = false;
+                isWorking = true;
+                const bot = mineflayer.createBot({
+                    host: 'play.shotbow.net',
+                    username: MCUN,
+                    password: MCPW,
+                    auth: 'microsoft',
+                    version: '1.12.2'
+                });
         
-            bot.on('spawn', async () => {
-                setTimeout(() => {
-                    reopen = false;
-                    bot.setQuickBarSlot(0);
-                    bot.activateItem();
-                    wait(500);
-                    bot.deactivateItem();
-                }, 1000);
-                console.log('spawn');
-            });
+                bot.on('spawn', async () => {
+                    setTimeout(() => {
+                        reopen = false;
+                        bot.setQuickBarSlot(0);
+                        bot.activateItem();
+                        wait(500);
+                        bot.deactivateItem();
+                    }, 1000);
+                    console.log('spawn');
+                });
         
-            bot.on('windowOpen', async (window) => {
-                //console.log('window opened');
-                //console.log(window.title);
-                if (window.title === '{"text":"Server Selector"}') {
-                    //console.log('clicking');
-                    connectAnni();
-                } else if (window.title === '{"text":"§7|§0Select Server§7|"}') {
-                    if (reopen === false) {
-                        reopen = true;
-                        bot.closeWindow(window);
-                        bot.simpleClick.rightMouse(22);
-                    } else {
-                        let matchList = [];
-                        let matchData = {};
-                        //console.log('opened anni vote menu');
-                        //console.log(window.slots);
-                        window.slots.map((item) => {
-                            if (item !== null && item.slot < 9) {
-                                if (item.displayName === 'Lime Dye') {
-                                    matchData['Joinablility'] = 'All';
-                                } else if (item.displayName === 'Light Blue Dye') {
-                                    matchData['Joinablility'] = 'Premium';
-                                } else if (item.displayName === 'Gray Dye') {
-                                    matchData['Joinablility'] = 'None';
+                bot.on('windowOpen', async (window) => {
+                    //console.log('window opened');
+                    //console.log(window.title);
+                    if (window.title === '{"text":"Server Selector"}') {
+                        //console.log('clicking');
+                        connectAnni();
+                    } else if (window.title === '{"text":"§7|§0Select Server§7|"}') {
+                        if (reopen === false) {
+                            reopen = true;
+                            bot.closeWindow(window);
+                            bot.simpleClick.rightMouse(22);
+                        } else {
+                            let matchList = [];
+                            let matchData = {};
+                            //console.log('opened anni vote menu');
+                            //console.log(window.slots);
+                            window.slots.map((item) => {
+                                if (item !== null && item.slot < 9) {
+                                    if (item.displayName === 'Lime Dye') {
+                                        matchData['Joinablility'] = 'All';
+                                    } else if (item.displayName === 'Light Blue Dye') {
+                                        matchData['Joinablility'] = 'Premium';
+                                    } else if (item.displayName === 'Gray Dye') {
+                                        matchData['Joinablility'] = 'None';
+                                    }
+                                    let mapRaw = item.nbt.value.display.value.Lore.value.value[0];
+                                    matchData['Map'] = formatMapText(mapRaw);
+                                    matchData['Players'] = item.nbt.value.display.value.Lore.value.value[1];
+                                    let phaseRaw = item.nbt.value.display.value.Lore.value.value[2];
+                                    matchData['Phase'] = formatPhaseText(phaseRaw);
+                                    let serverRaw = item.nbt.value.display.value.Name.value;
+                                    matchData['Server'] = formatServerName(serverRaw);
+                                    let matchDataCopy = {};
+                                    matchList.push(Object.assign(matchDataCopy, matchData));
                                 }
-                                let mapRaw = item.nbt.value.display.value.Lore.value.value[0];
-                                matchData['Map'] = formatMapText(mapRaw);
-                                matchData['Players'] = item.nbt.value.display.value.Lore.value.value[1];
-                                let phaseRaw = item.nbt.value.display.value.Lore.value.value[2];
-                                matchData['Phase'] = formatPhaseText(phaseRaw);
-                                let serverRaw = item.nbt.value.display.value.Name.value;
-                                matchData['Server'] = formatServerName(serverRaw);
-                                let matchDataCopy = {};
-                                matchList.push(Object.assign(matchDataCopy, matchData));
-                            }
-                        });
-                        //console.log(matchList);
-                        bot.end();
-                        resolve(matchList);
+                            });
+                            //console.log(matchList);
+                            bot.end();
+                            isWorking = false;
+                            resolve(matchList);
+                        }
                     }
-                }
-                bot.closeWindow(window);
-            });
+                    bot.closeWindow(window);
+                });
             
-            function connectAnni() {
-                bot.simpleClick.leftMouse(5);
-            }
-        
-            function formatPhaseText(phaseRaw) {
-                if (phaseRaw.startsWith('§ePhase')) {   // after Phase 3
-                    phaseRaw = phaseRaw.replace('§e', '');
-                } else if (phaseRaw.startsWith('§bPhase 3')) {  // Phase 3
-                    phaseRaw = phaseRaw.replace('§r (§bPremium§r join privilege)', '').replace('§b', '');
-                } else if (phaseRaw.startsWith('§aPhase')) { // before Phase 3
-                    phaseRaw = phaseRaw.replace('§a', '');
-                } else if (phaseRaw === '§aPre game') { // before vote
-                    phaseRaw = 'Before Voting';
-                } else if (phaseRaw.startsWith('§aStarts in')) {
-                    phaseRaw = phaseRaw.replace('§a', '') + ' seconds';
-                } else if (phaseRaw === '§eEnding') {
-                    phaseRaw = 'Ending';
+                function connectAnni() {
+                    bot.simpleClick.leftMouse(5);
                 }
-                return phaseRaw;
-            }
         
-            function formatMapText(mapRaw) {
-                if (mapRaw.startsWith('Map:')) {
-                    mapRaw = mapRaw.replace('Map: ', '');
-                } // add voting?
-                return mapRaw;
-            }
-        
-            function formatServerName(serverRaw) {
-                serverRaw = serverRaw.replace('§r§n', '');
-                return serverRaw;
-            }
-        }).then((data) => {
-            //console.log(data);
-            data.map((server) => {
-                let name = server['Server'];
-                let value = '';
-                if (server['Map'].startsWith('Voting:')) {
-                    value = '```' + server['Map'] + '\nPlayers: ' + server['Players'] + '\nState: ' + server['Phase'] + '```';
-                } else {
-                    value = '```Map:     ' + server['Map'] + '\nPlayers: ' + server['Players'] + '\nState:   ' + server['Phase'] + '```';
+                function formatPhaseText(phaseRaw) {
+                    if (phaseRaw.startsWith('§ePhase')) {   // after Phase 3
+                        phaseRaw = phaseRaw.replace('§e', '');
+                    } else if (phaseRaw.startsWith('§bPhase 3')) {  // Phase 3
+                        phaseRaw = phaseRaw.replace('§r (§bPremium§r join privilege)', '').replace('§b', '');
+                    } else if (phaseRaw.startsWith('§aPhase')) { // before Phase 3
+                        phaseRaw = phaseRaw.replace('§a', '');
+                    } else if (phaseRaw === '§aPre game') { // before vote
+                        phaseRaw = 'Before Voting';
+                    } else if (phaseRaw.startsWith('§aStarts in')) {
+                        phaseRaw = phaseRaw.replace('§a', '') + ' seconds';
+                    } else if (phaseRaw === '§eEnding') {
+                        phaseRaw = 'Ending';
+                    }
+                    return phaseRaw;
                 }
-                fieldRaw['name'] = name;
-                fieldRaw['value'] = value;
-                let fieldCopy = {};
-                fieldsRaw.push(Object.assign(fieldCopy, fieldRaw));
+        
+                function formatMapText(mapRaw) {
+                    if (mapRaw.startsWith('Map:')) {
+                        mapRaw = mapRaw.replace('Map: ', '');
+                    } // add voting?
+                    return mapRaw;
+                }
+        
+                function formatServerName(serverRaw) {
+                    serverRaw = serverRaw.replace('§r§n', '');
+                    return serverRaw;
+                }
+            }).then((data) => {
+                //console.log(data);
+                data.map((server) => {
+                    let name = server['Server'];
+                    let value = '';
+                    if (server['Map'].startsWith('Voting:')) {
+                        value = '```' + server['Map'] + '\nPlayers: ' + server['Players'] + '\nState: ' + server['Phase'] + '```';
+                    } else {
+                        value = '```Map:     ' + server['Map'] + '\nPlayers: ' + server['Players'] + '\nState:   ' + server['Phase'] + '```';
+                    }
+                    fieldRaw['name'] = name;
+                    fieldRaw['value'] = value;
+                    let fieldCopy = {};
+                    fieldsRaw.push(Object.assign(fieldCopy, fieldRaw));
+                });
+                embedObject = {
+                    title: 'Matches',
+                    fields: fieldsRaw
+                };
+            }).then(() => {
+                interaction.editReply({ embeds: [embedObject] });
             });
-            embedObject = {
-                title: 'Matches',
-                fields: fieldsRaw
-            };
-        }).then(() => {
-            interaction.editReply({ embeds: [embedObject] });
-        });
+        }
     }
     if (interaction.commandName === 'stats') {
         await interaction.deferReply();
@@ -198,6 +217,49 @@ client.on('interactionCreate', async (interaction) => {
             .setFooter({ text: 'Info from Shotbow.net', iconURL: 'https://shotbow.net/forum/styles/fusiongamer/xenforo/avatars/avatar_l.png' });
 
         await interaction.editReply({embeds: [embedObject]});
+    }
+    if (interaction.commandName === 'namemc') {
+        await interaction.deferReply();
+        let username = interaction.options.getString('mcid');
+        let embedString = '```';
+        let embedObject;
+        axios.post('https://api.mojang.com/profiles/minecraft', [username]).then((rawUUIDData) => {
+            if (Object.keys(rawUUIDData.data).length === 0) {
+                interaction.editReply({ content: 'Invalid MCID!' });
+            } else {
+                const uuid = rawUUIDData.data[0]["id"];
+                username = rawUUIDData.data[0]["name"];
+                axios.get('https://api.mojang.com/user/profiles/' + uuid + '/names').then((rawNameHistoryData) => {
+                    for (let i = 0; i < rawNameHistoryData.data.length; i++) {
+                        let mcid = rawNameHistoryData.data[i]['name'];
+                        embedString += mcid;
+                        embedString = insertSpace(embedString, 3);
+                        let dateraw = rawNameHistoryData.data[i]['changedToAt'];
+                        if (dateraw !== undefined) {
+                            let date = new Date(dateraw);
+                            embedString += ('Changed At ' + date.getFullYear().toString() + '/' + date.getMonth().toString() + '/' + date.getDay().toString());
+                        } else {
+                            embedString += 'Original MCID';
+                        }
+                        embedString += '\n';
+                    }
+                    embedString += '```';
+                    embedObject = new EmbedBuilder()
+                        //.setTitle('Name History')
+                        .setAuthor({ name: username + '\'s Name History', url: 'https://ja.namemc.com/profile/' + uuid })
+                        .setThumbnail('https://crafatar.com/avatars/' + uuid)
+                        .addFields({ name: 'History', value: embedString });
+                }).then(() => {
+                    interaction.editReply({ embeds: [embedObject] });
+                });
+            }
+        });
+        const insertSpace = (str, num) => {
+            for (let i = 0; i < num; i++) {
+                str += ' ';
+            }
+            return str;
+        }
     }
 });
 
